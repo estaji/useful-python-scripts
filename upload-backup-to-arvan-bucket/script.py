c0b61f98ddd2b2fd58183613fb179e1171b9dba4
Omid Estaji <estaji.work@gmail.com>
2025-03-04 10:23:31 +0330

    Delete old files on the remote bucket

--- upload-backup-to-arvan-bucket/script.py
+++ upload-backup-to-arvan-bucket/script.py
@@ -1,6 +1,7 @@
 import boto3
 import logging
 import sys
+from datetime import datetime
 from botocore.exceptions import ClientError
 
 # Configure logging
@@ -13,6 +14,7 @@ bucket_name = 'BUCKET_NAME'
 bucket_endpoint_url = 'https://s3.ir-thr-at1.arvanstorage.ir'
 bucket_access_key = 'ACCESS_KEY'
 bucket_secret_key = 'SECRET_KEY'
+remove_files_older_than = 40
 
 # Functions
 def upload_bak():
@@ -41,5 +43,32 @@ def upload_bak():
        except ClientError as e:
            logging.error(e)
 
+def remove_old_files():
+    try:
+       client = boto3.client(
+           's3',
+           endpoint_url = bucket_endpoint_url,
+           aws_access_key_id = bucket_access_key,
+           aws_secret_access_key = bucket_secret_key,
+       )    
+
+       response = client.list_objects(Bucket=bucket_name)
+
+       today_date_time = datetime.now().replace(tzinfo=None)
+
+       for file in response.get("Contents"):
+           file_name = file.get("Key")
+           modified_time = file.get("LastModified").replace(tzinfo=None)
+                
+           difference_days_delta = today_date_time - modified_time
+           difference_days = difference_days_delta.days
+           if difference_days > remove_files_older_than:
+               client.delete_object(Bucket=bucket_name,Key=file_name)
+
+    except Exception as exc:
+       logging.error(exc)
+
+
 # Main
 upload_bak()
+remove_old_files()
